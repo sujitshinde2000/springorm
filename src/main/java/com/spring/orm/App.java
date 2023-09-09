aa45c808390aa21c5f20d2d24c9f348f6cc87c2c
sujitshinde1000 <sujitshinde2000@gmail.com>
2023-09-09 17:55:10 +0530

    first commit

---
+++ src/main/java/com/spring/orm/App.java
@@ -0,0 +1,27 @@
+package com.spring.orm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.orm.dao.StudentDao;
+import com.spring.orm.entities.Student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       
+    	ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+    	
+    	StudentDao studentDao = context.getBean("studentDao",StudentDao.class);
+    	Student student = new Student(1234, "Sujit Shinde", "Baramati");
+    	int r = studentDao.insert(student);
+    	
+    	System.out.println("done" +r);
+    	
+    }
+}
---
+++ src/main/java/com/spring/orm/dao/StudentDao.java
@@ -0,0 +1,34 @@
+package com.spring.orm.dao;
+
+import javax.transaction.Transactional;
+
+import org.springframework.orm.hibernate5.HibernateTemplate;
+
+import com.spring.orm.entities.Student;
+
+public class StudentDao {
+
+	private HibernateTemplate hibernateTemplate;
+	
+	//save student
+	@Transactional
+	public int insert(Student student) {
+		
+		//insert
+		Integer i = (Integer) this.hibernateTemplate.save(student);
+	
+		return i;
+	}
+
+	public HibernateTemplate getHibernateTemplate() {
+		return hibernateTemplate;
+	}
+
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
+	}
+	
+	
+	
+	
+}
